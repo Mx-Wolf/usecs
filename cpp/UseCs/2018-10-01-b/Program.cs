4d300684d0edde21e99dc47f51cb789c979325d0
Maxim Volkov <mx_ruz_net@hotmail.com>
2018-09-01 11:26:27 +0300

    getting ready to change program to solve the full problem

--- cpp/UseCs/2018-10-01-b/Program.cs
+++ cpp/UseCs/2018-10-01-b/Program.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
@@ -13,16 +14,18 @@ namespace _2018_10_01_b
             var result = 0;
             while (0<n--)
             {
-                result += MaxOfTriplet();
+                var triplet = GetTriplet().OrderBy(a=>a).ToArray();
+                result += triplet[2];
             }
             Console.WriteLine($"Максимальное значение суммы чисел, взятых по одному из каждой тройки: {result}");
             Console.WriteLine($"Результат делится на три: {(result%3==0?"Да":"Нет")}");
         }
 
-        private static int MaxOfTriplet()
+        private static IEnumerable<int> GetTriplet()
         {
             var line = ReadNextNonEmptyLine();
-            return line.Split(' ').Select(int.Parse).Max();
+            var triplet = line.Split(' ').Select(int.Parse);
+            return triplet;
         }
 
         private static int ReadTripletCount()
